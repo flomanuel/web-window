ab1825e91207928de62229e486cbb2dca6a1919f
flomanuel <flomanuel.github@gmail.com>
2021-07-06 10:22:09 +0200

    4.0.0-alpha: feature to save new website entries
    added:
    - settings controller
    - dependency 'electron-settings' for saving entries
    - frontend

---
+++ src/frontend/settings/preload.js
@@ -0,0 +1,43 @@
+const {contextBridge, ipcRenderer} = require('electron')
+const wwEvents = require('../../constants/wwEvents')
+
+class SettingsPreload {
+    init() {
+        this.prepareEvents();
+    }
+
+    prepareEvents() {
+        ipcRenderer.send(wwEvents.SETTINGS_WINDOW_REQ_SETTINGS.toString());
+        const websites = new Promise(resolve => {
+            ipcRenderer.on(wwEvents.SETTINGS_WINDOW_REQ_SETTINGS_RESPONSE.toString(), (e, args) => {
+                resolve(args && args.websites ? args.websites : [])
+            })
+        });
+
+        contextBridge.exposeInMainWorld('electron', {
+            'websiteEntries': websites,
+            'clearWebsites': () => {
+                ipcRenderer.send(wwEvents.SETTINGS_WINDOW_REQ_REMOVE_WEBSITES.toString(),)
+                return new Promise(resolve => {
+                    ipcRenderer.on(wwEvents.SETTINGS_WINDOW_REQ_REMOVE_WEBSITES_RESPONSE.toString(), (e, args) => {
+                        resolve(args);
+                    })
+                })
+            },
+            'saveNewEntry': (title, url, imgPath) => {
+                ipcRenderer.send(wwEvents.SETTINGS_WINDOW_REQ_SAVE_SETTINGS.toString(), {
+                    title: title,
+                    url: url,
+                    imgPath: imgPath
+                })
+                return new Promise(resolve => {
+                    ipcRenderer.on(wwEvents.SETTINGS_WINDOW_REQ_SAVE_SETTINGS_RESPONSE.toString(), (e, args) => {
+                        resolve(args);
+                    })
+                })
+            }
+        });
+    }
+}
+
+new SettingsPreload().init();
